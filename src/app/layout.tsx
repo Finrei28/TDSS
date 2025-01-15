b9d537cee66e65c8ce5b4ea92e086285e714ad12
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-01-15 17:31:15 +1300

    fixed href for favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="favicon.png" />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
