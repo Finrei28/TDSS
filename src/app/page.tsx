5f8b3e0ab6d6d5cba02e36a8463e92587163feef
Finrei28 <161308914+Finrei28@users.noreply.github.com>
2025-02-04 18:01:17 +1300

    added Head tag on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/homePage/footer"
 import ContactModal from "@/components/homePage/contactModal"
 import { useEffect, useState } from "react"
 import TopStrategies from "@/components/homePage/TopStrategies"
+import Head from "next/head"
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -23,58 +24,67 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="bg-slate-50">
-      <section>
-        <MaxWidthWapper className="pb-24 pt-10 lg:grid sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-            <div className="relative mx-auto text-center flex flex-col justify-center items-center">
-              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-1xl md:text-2xl lg:text-3xl">
-                Stuck on a Tower Defense Simulator map?
-              </h1>
-              <p className="relative w-fit tracking-tight text-balance mt-1 !leading-tight text-gray-900 text-sm md:text-base lg:text-lg">
-                Browse through TDS strategies to help you win your next game!
-              </p>
-              <div className="flex flex-col sm:flex-row mt-20 gap-10">
-                <Link href="/special">
-                  <div className="object-cover duration-300 md:hover:scale-105">
-                    <span className="text-orange-400 text-xl">Special</span>
-                    <img
-                      src="/TDS Special.png"
-                      alt="TDS Special mode"
-                      className="rounded-sm w-full max-w-xs border-2 border-orange-400"
-                    />
-                  </div>
-                </Link>
-                <Link href="/normal">
-                  <div className="object-cover duration-300 md:hover:scale-105">
-                    <span className="text-green-500 text-xl">Normal</span>
-                    <img
-                      src="/TDS Normal.png"
-                      alt="TDS Normal mode"
-                      className="rounded-sm w-full max-w-xs border-2 border-green-500"
-                    />
-                  </div>
-                </Link>
-                <Link href="/hardcore">
-                  <div className="object-cover duration-300 md:hover:scale-105">
-                    <span className="text-purple-600 text-xl">Hardcore</span>
-                    <img
-                      src="/TDS Hardcore.png"
-                      alt="TDS Hardcore mode"
-                      className="rounded-sm w-full max-w-xs border-2 border-purple-600"
-                    />
-                  </div>
-                </Link>
+    <>
+      <Head>
+        <title>Tower Defense Simulator Strategies</title>
+        <meta
+          name="TDS Strategies - The Best Tower Defense Strategies"
+          content="Find the best strategies for Tower Defense Simulator. Share your own strategies with the community to help others!"
+        />
+      </Head>
+      <div className="bg-slate-50">
+        <section>
+          <MaxWidthWapper className="pb-24 pt-10 lg:grid sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+            <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+              <div className="relative mx-auto text-center flex flex-col justify-center items-center">
+                <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-1xl md:text-2xl lg:text-3xl">
+                  Stuck on a Tower Defense Simulator map?
+                </h1>
+                <p className="relative w-fit tracking-tight text-balance mt-1 !leading-tight text-gray-900 text-sm md:text-base lg:text-lg">
+                  Browse through TDS strategies to help you win your next game!
+                </p>
+                <div className="flex flex-col sm:flex-row mt-20 gap-10">
+                  <Link href="/special">
+                    <div className="object-cover duration-300 md:hover:scale-105">
+                      <span className="text-orange-400 text-xl">Special</span>
+                      <img
+                        src="/TDS Special.png"
+                        alt="TDS Special mode"
+                        className="rounded-sm w-full max-w-xs border-2 border-orange-400"
+                      />
+                    </div>
+                  </Link>
+                  <Link href="/normal">
+                    <div className="object-cover duration-300 md:hover:scale-105">
+                      <span className="text-green-500 text-xl">Normal</span>
+                      <img
+                        src="/TDS Normal.png"
+                        alt="TDS Normal mode"
+                        className="rounded-sm w-full max-w-xs border-2 border-green-500"
+                      />
+                    </div>
+                  </Link>
+                  <Link href="/hardcore">
+                    <div className="object-cover duration-300 md:hover:scale-105">
+                      <span className="text-purple-600 text-xl">Hardcore</span>
+                      <img
+                        src="/TDS Hardcore.png"
+                        alt="TDS Hardcore mode"
+                        className="rounded-sm w-full max-w-xs border-2 border-purple-600"
+                      />
+                    </div>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </MaxWidthWapper>
-      </section>
-      <section className="bg-slate-200">
-        <TopStrategies />
-      </section>
-      <ContactModal isOpen={isModalOpen} onClose={closeModal} />
-      <Footer openModal={openModal} />
-    </div>
+          </MaxWidthWapper>
+        </section>
+        <section className="bg-slate-200">
+          <TopStrategies />
+        </section>
+        <ContactModal isOpen={isModalOpen} onClose={closeModal} />
+        <Footer openModal={openModal} />
+      </div>
+    </>
   )
 }
